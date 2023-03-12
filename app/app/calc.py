cd1b592e2d63ed764e3eafe2b44ac0c2ec62e1f7
abubakarA-Dot <abubakarbryar@gmail.com>
2023-03-12 16:40:30 +0500

    Fixed lint issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,11 @@
 """Calculate 2 numbers"""
 
-def add_numbers(x,y):
+
+def add_numbers(x, y):
     """Add numbers"""
     return x + y
 
-def substract_numbers(x,y):
+
+def substract_numbers(x, y):
     """Substract numbers"""
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,15 +3,15 @@ from django.test import SimpleTestCase
 from . import calc
 
 class CalculateTests(SimpleTestCase):
-    
+
     def test_add_numbers(self):
         """Test"""
 
-        res = calc.add_numbers(5,6)
+        res = calc.add_numbers(5, 6)
         self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
         """Test"""
 
-        res = calc.substract_numbers(25,36)
+        res = calc.substract_numbers(25, 36)
         self.assertEqual(res, 11)
