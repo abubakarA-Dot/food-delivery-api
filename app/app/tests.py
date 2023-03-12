e31fa9443bf555d244a0d5f99caea66b2978d4df
abubakarA-Dot <abubakarbryar@gmail.com>
2023-03-12 16:58:35 +0500

    Fixed lint issues

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,12 +7,10 @@ class CalculateTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test"""
-
         res = calc.add_numbers(5, 6)
         self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
         """Test"""
-
         res = calc.substract_numbers(25, 36)
         self.assertEqual(res, 11)
