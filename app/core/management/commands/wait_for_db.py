ac55f83488aded4f367fa03d879484cbf9270692
abubakarA-Dot <abubakarbryar@gmail.com>
2023-03-12 16:33:33 +0500

    Fixed lint issues

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand
 import time
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
-from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for db"""
