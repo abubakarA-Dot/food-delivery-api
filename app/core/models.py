08d1d8537aa95de1f4134bb1cad0e37528c7c046
abubakarA-Dot <abubakarbryar@gmail.com>
2023-03-28 18:08:08 +0500

    Added Test cases for django admin and custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -12,8 +12,21 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an Email")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, save and return a new super user"""
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -22,8 +35,8 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    namr = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
