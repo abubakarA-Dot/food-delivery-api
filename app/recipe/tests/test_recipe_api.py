83af82bb352fa032ef3e90b29745b9d6ca319cb7
abubakarA-Dot <abubakarbryar@gmail.com>
2023-07-21 18:35:44 +0500

    create a new recipe unit test

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -100,4 +100,20 @@ class PrivateRecipeApiTests(TestCase):
         
         serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_recipe(self):
+        """Test creating a new recipe."""
+        payload = {
+            'title': 'Sample recipe',
+            'time_minutes': 30,
+            'price': Decimal('5.99'),
+        }
+        print("\n Payload \n", payload)
+        res = self.client.post(RECIPE_URLS, payload)
+        
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        for key, value in payload.items():
+            self.assertEqual(getattr(recipe, key), value)
+        self.assertEqual(recipe.user, self.user)
         
