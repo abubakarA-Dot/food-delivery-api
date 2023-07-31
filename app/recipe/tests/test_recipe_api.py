be48f0e720949f7d12dbbaf6e3a4e4f384df9eff
abubakarA-Dot <abubakarbryar@gmail.com>
2023-07-31 08:48:23 +0500

    Fix flake 8 and pep8 issues

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -51,7 +51,7 @@ class PublicRecipeTests(TestCase):
     def test_auth_required(self):
         """Test Auth is required when calling recipe API"""
         res = self.client.get(RECIPE_URLS)
-        
+
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -67,14 +67,15 @@ class PrivateRecipeApiTests(TestCase):
         """Test retrieving a list of recipes"""
         create_recipe(user=self.user)
         create_recipe(user=self.user)
-        
+
         res = self.client.get(RECIPE_URLS)
-        
+
         recipe = Recipe.objects.all().order_by('-id')
         serializer = RecipeSerializer(recipe, many=True)
-        
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
 
     def test_recipe_limited_to_user(self):
         """Test list of recipes is limited to authenticated user"""
@@ -83,7 +84,7 @@ class PrivateRecipeApiTests(TestCase):
         create_recipe(user=self.user)
 
         res = self.client.get(RECIPE_URLS)
-        
+
         recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
@@ -92,12 +93,13 @@ class PrivateRecipeApiTests(TestCase):
     def test_get_recipe_detail(self):
         """Test get recipe detail"""
         recipe = create_recipe(user=self.user)
-        
+
         url = detail_url(recipe.id)
         res = self.client.get(url)
-        
+
         serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
+
 
     def test_create_recipe(self):
         """Test creating a recipe."""
@@ -107,14 +109,14 @@ class PrivateRecipeApiTests(TestCase):
             'price': Decimal('5.99'),
         }
         res = self.client.post(RECIPE_URLS, payload)
-        
+
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         recipe = Recipe.objects.get(id=res.data['id'])
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
-        
-        
+
+
     def test_partial_update(self):
         """Test partial update of a recipe."""
         original_link = "https://example.com/recipe.pdf"
@@ -128,7 +130,7 @@ class PrivateRecipeApiTests(TestCase):
         }
         url = detail_url(recipe.id)
         res = self.client.patch(url, payload)
-        
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         recipe.refresh_from_db()
         self.assertEqual(recipe.title, payload['title'])
@@ -154,15 +156,15 @@ class PrivateRecipeApiTests(TestCase):
         }
         url = detail_url(recipe.id)
         res = self.client.put(url, payload)
-        
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         recipe.refresh_from_db()
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
-        
+
         # or next approach
-        
+
         # self.assertEqual(recipe.title, payload['title'])
         # self.assertEqual(recipe.link, original_link)
         # self.assertEqual(recipe.description, payload['description'])
@@ -183,7 +185,7 @@ class PrivateRecipeApiTests(TestCase):
         }
         url = detail_url(recipe.id)
         res = self.client.patch(url, payload)
-        
+
         recipe.refresh_from_db()
         self.assertEqual(recipe.user, self.user)
 
@@ -193,7 +195,7 @@ class PrivateRecipeApiTests(TestCase):
         recipe = create_recipe(user=self.user)
         url = detail_url(recipe.id)
         res = self.client.delete(url)
-        
+
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Recipe.objects.filter(id=recipe.id).first())
 
@@ -207,6 +209,6 @@ class PrivateRecipeApiTests(TestCase):
         recipe = create_recipe(user=new_user)
         url = detail_url(recipe.id)
         res = self.client.delete(url)
-        
+
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
         self.assertTrue(Recipe.objects.filter(id=recipe.id).first())
